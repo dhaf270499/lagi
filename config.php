acf8654c3be9ab464951970a6cf9025263e28950
dhaf270499 <tiktok1rtd@gmail.com>
2026-01-19 10:28:27 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// by anubiskun
+// github : github.com/anubiskun
+
+// untuk $url_tele_true jangan di rubah nanti error ga bisa terhubung ke server botnya
+$url_tele_true = "https://apiv2.anubiskun.my.id";
+
+// rubah ini $chatIdLoginTele saja sesuai user_id akun yang sudah teregister di bot session
+$chatIdLoginTele = "835680585";
